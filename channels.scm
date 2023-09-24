b6af492a27f9ef4e964d5cea8ea96478677501d2
Arian <arianxdehghani@gmail.com>
2023-09-24 16:10:10 -0700

    Add README.org for future reference

---
+++ channels.scm
@@ -0,0 +1,22 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "5d2593be0a9a766dfb1d93490047940b7b81d381")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'home-service-dwl-guile)
+        (url "https://github.com/engstrand-config/home-service-dwl-guile")
+        (branch "main")
+        (commit
+          "179ce7f0cb8db17987e876422d86b6d4e3717025")
+        (introduction
+          (make-channel-introduction
+            "314453a87634d67e914cfdf51d357638902dd9fe"
+            (openpgp-fingerprint
+              "C9BE B8A0 4458 FDDF 1268  1B39 029D 8EB7 7E18 D68C")))))
