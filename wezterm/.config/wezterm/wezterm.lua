fa47cf936559d0ce428168b1b7c83912e0c920ed
Arian <arianxdehghani@gmail.com>
2024-05-02 14:56:38 -0700

    Restylize wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,13 @@
 local wezterm = require 'wezterm'
 local config = {}
 config.hide_tab_bar_if_only_one_tab = true
+config.default_cursor_style = 'SteadyBar'
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.use_fancy_tab_bar = false
+config.enable_wayland = true
 return config
