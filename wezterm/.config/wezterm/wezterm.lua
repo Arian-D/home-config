3ad4133505c6f221e0afbbb88b50315303b0d505
Arian <arianxdehghani@gmail.com>
2024-09-13 14:29:17 -0700

    Increased max wezterm tab width size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 48
 config.default_cursor_style = 'SteadyBar'
 config.window_padding = {
   left = 0,
